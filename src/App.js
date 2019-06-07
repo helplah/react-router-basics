f62e7c7a8bac94f1f28709622a67f3f890e9606e
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-07 15:45:26 +0800

    Add folder where we played around with react router basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+
+const Home = props => {
+  console.log(props);
+  return <h1>Home</h1>;
+};
+
+const About = props => {
+  console.log(props);
+  return <h1>About</h1>;
+};
+
+const FAQ = () => {
+  return <h1>FAQ</h1>;
+};
+
+const Content = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const Movie = ({ match }) => {
+  const movieId = Number(match.params.movieId);
+  const movies = [{ id: 1, title: "Endgame" }, { id: 2, title: "Avengers" }];
+  const movie = movies.find(m => m.id === movieId);
+
+  return <h1>{movie && movie.title}</h1>;
+};
 
 function App() {
+  const movieId = 1;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/faq">FAQ</Link>
+          </li>
+          <li>
+            <Link to="/content">Content</Link>
+          </li>
+          <li>
+            <Link to={`/movies/${movieId}`}>Movie</Link>
+          </li>
+        </nav>
+        <main>
+          <Route exact={true} path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/faq" component={FAQ} />
+          {/* The reason why we use render in Route is to pass additional props */}
+          <Route
+            path="/content"
+            render={() => <Content text="My awesome content" />}
+          />
+          <Route path="/movies/:movieId" component={Movie} />
+        </main>
+      </div>
+    </Router>
   );
 }
 
